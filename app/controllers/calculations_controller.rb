1994b4056491442f2a0a80a609f726e62db6bc09
vikramraju89 <vraju2016@kellogg.northwestern.edu>
2015-05-11 09:22:09 -0500

    Complete RCAV Software Exercise

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -1,10 +1,22 @@
 class CalculationsController < ApplicationController
-  def instructions
+  def square
+    @number =  params["number"]
+    @inumber = @number.to_i
+    @dasquare = (@inumber)**2
   end
 
   def sqrt
+    @number = params["number"]
+    @inumber = @number.to_i
+    @daroot = Math.sqrt(@inumber)
   end
 
   def pmt
+    @apr = params["interest"].to_f/10
+    @rate = @apr/12/100
+    @payments = params["nopayments"].to_f
+    @terms = @payments
+    @principal = params["principal"].to_f
+    @monthly_payment =  (@principal*(((1+@rate)**@terms)*@rate)/(((1+@rate)**@terms)-1)).round(3)
   end
 end
